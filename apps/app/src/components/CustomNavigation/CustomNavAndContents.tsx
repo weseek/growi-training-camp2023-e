5b1db3a4e0c675ee7e1544d7faa8022ceb2a95d4
yusuke-sak <yusuke-sak@weseek.co.jp>
2023-10-11 13:17:19 +0000

    add temporary code

--- apps/app/src/components/CustomNavigation/CustomNavAndContents.tsx
+++ apps/app/src/components/CustomNavigation/CustomNavAndContents.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode, useState } from 'react';
 
+import { useRouter } from 'next/router';
+
 import CustomNav, { CustomNavTab, CustomNavDropdown } from './CustomNav';
 import CustomTabContent from './CustomTabContent';
 
 type CustomNavAndContentsProps = {
   navTabMapping: any,
   defaultTabIndex?: number,
-  navigationMode?: 'both' | 'tab' | 'dropdown',
+  navigationMode?: 'both' | 'tab' | 'dropdown'|'null',
   tabContentClasses?: string[],
   breakpointToHideInactiveTabsDown?: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
   navRightElement?: ReactNode
@@ -14,33 +16,47 @@ type CustomNavAndContentsProps = {
 
 
 const CustomNavAndContents = (props: CustomNavAndContentsProps): JSX.Element => {
+  const router = useRouter();
+  const currentPage = router.pathname;
+  console.log('currentPage', currentPage);
+
   const {
     navTabMapping, defaultTabIndex, navigationMode = 'tab', tabContentClasses = ['p-4'], breakpointToHideInactiveTabsDown, navRightElement,
   } = props;
   const [activeTab, setActiveTab] = useState(Object.keys(props.navTabMapping)[defaultTabIndex || 0]);
 
   let SelectedNav;
+  if (navigationMode === 'null') {
+    SelectedNav = null;
+    console.log('仮で文字列のnull追加');
+  }
   switch (navigationMode) {
     case 'tab':
+      console.log('tab');
+      // SelectedNav = null;
       SelectedNav = CustomNavTab;
       break;
     case 'dropdown':
+      console.log('dropdown');
       SelectedNav = CustomNavDropdown;
       break;
     case 'both':
+      console.log('both');
       SelectedNav = CustomNav;
       break;
   }
 
   return (
     <>
-      <SelectedNav
-        activeTab={activeTab}
-        navTabMapping={navTabMapping}
-        onNavSelected={setActiveTab}
-        breakpointToHideInactiveTabsDown={breakpointToHideInactiveTabsDown}
-        navRightElement={navRightElement}
-      />
+      {SelectedNav ? (
+        <SelectedNav
+          activeTab={activeTab}
+          navTabMapping={navTabMapping}
+          onNavSelected={setActiveTab}
+          breakpointToHideInactiveTabsDown={breakpointToHideInactiveTabsDown}
+          navRightElement={navRightElement}
+        />
+      ) : null}
       <CustomTabContent activeTab={activeTab} navTabMapping={navTabMapping} additionalClassNames={tabContentClasses} />
     </>
   );
