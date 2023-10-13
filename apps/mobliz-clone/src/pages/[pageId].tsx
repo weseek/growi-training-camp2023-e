bf1e524e005c0db404e2610baca0ab7e9c6d1d9a
yusuke-sak <yusuke-sak@weseek.co.jp>
2023-10-13 00:41:22 +0000

    enable to show title

--- apps/mobliz-clone/src/pages/[pageId].tsx
+++ apps/mobliz-clone/src/pages/[pageId].tsx
@@ -16,13 +16,13 @@ const DetailPage: NextPage<Props> = (props: Props) => {
   const pageId = router.query.pageId ?? props.pageId;
   const [error, setError] = useState<string>();
   const [htmlString, setHTMLString] = useState();
-  const [page, setPage] = useState<any>();
+  const [resData, setResData] = useState<any>();
 
   useEffect(() => {
     axios.get(`${process.env.NEXT_PUBLIC_APP_SITE_URL}/_cms/${pageId}.json`)
       .then((response) => {
         setHTMLString(response.data.htmlString);
-        setPage(response.data.page);
+        setResData(response.data);
       })
       .catch((error) => {
         setError(`データの取得に失敗しました。\n${JSON.stringify(error)}`);
@@ -40,14 +40,14 @@ const DetailPage: NextPage<Props> = (props: Props) => {
           ) : (
             <>
               <div className="list-inline d-flex mb-4">
-                <p className="me-4"> {dateFnsFormat(new Date(page.createdAt), 'yyyy.MM.dd')}</p>
-                <div> {dateFnsFormat(new Date(page.updatedAt), 'yyyy.MM.dd')}</div>
+                <p className="me-4"> {dateFnsFormat(new Date(resData.page.createdAt), 'yyyy.MM.dd')}</p>
+                <div> {dateFnsFormat(new Date(resData.page.updatedAt), 'yyyy.MM.dd')}</div>
               </div>
-              <h2 className="pb-5 fw-bold">{page.title}</h2>
+              <h2 className="pb-5 fw-bold">{resData.title}</h2>
               {parse(htmlString)}
               <hr />
-              <img src={page.creator.imageUrlCached} width="100" height="100" alt="" />
-              <p><strong>{page.creator.name}</strong></p>
+              <img src={resData.page.creator.imageUrlCached} width="100" height="100" alt="" />
+              <p><strong>{resData.page.creator.name}</strong></p>
             </>
           )}
         </>
